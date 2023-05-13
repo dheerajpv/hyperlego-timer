cd78903614535c2982f3c67913fb7d55eb04d193
Dheeraj Prakash <dheerajpv16@gmail.com>
2023-05-13 12:23:02 -0400

    it makes noises now

--- index.js
+++ index.js
@@ -4,15 +4,40 @@ const pause = document.querySelector("#timer-pause");
 const resume = document.querySelector("#timer-resume");
 const reset = document.querySelector("#timer-reset");
 
+// disable controls until all audio loads
+start.enabled = false;
+reset.enabled = false;
+
+const startAudio = new Audio("/assets/CHARGE.wav");
+const warnAudio = new Audio("/assets/TimeToRun.wav");
+const endAudio = new Audio("/assets/BUZZER.wav");
+const errorAudio = new Audio("/assets/FOGHORN.wav");
+
+await Promise.all(
+    [startAudio, warnAudio, endAudio, errorAudio].map(
+        (a) =>
+            new Promise((resolve) =>
+                a.addEventListener("canplaythrough", resolve)
+            )
+    )
+);
+
+start.enabled = true;
+reset.enabled = true;
+
 const timerLength = 150;
 
 let remaining = timerLength;
 
-let timerInterval;
+let timerInterval = null;
+let playedWarn = false;
 
 function interval() {
     remaining -= 1;
-    if (remaining < 0) clearInterval(interval);
+    if (remaining <= 0) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+    }
     update();
 }
 
@@ -20,20 +45,33 @@ function update() {
     const min = Math.floor(remaining / 60);
     const sec = `${remaining % 60}`.padStart(2, "0");
     timer.textContent = `${min}:${sec}`;
+
     timer.className = "";
     if (remaining > 30) timer.classList.add("good");
-    else if (remaining > 0) timer.classList.add("warn");
-    else timer.classList.add("stop");
+    else if (remaining > 0) {
+        timer.classList.add("warn");
+        if (!playedWarn) {
+            warnAudio.play();
+            playedWarn = true;
+        }
+    } else {
+        timer.classList.add("stop");
+        endAudio.play();
+    }
 }
 
 start.addEventListener("click", () => {
+    startAudio.play();
     timerInterval = setInterval(interval, 1000);
     pause.disabled = false;
     start.disabled = true;
+    playedWarn = false;
 });
 
 pause.addEventListener("click", () => {
     clearInterval(timerInterval);
+    timerInterval = null;
+    errorAudio.play();
     timer.classList = "";
     timer.classList.add("stop");
     pause.disabled = true;
@@ -41,14 +79,17 @@ pause.addEventListener("click", () => {
 });
 
 resume.addEventListener("click", () => {
+    startAudio.play();
     timerInterval = setInterval(interval, 1000);
     update();
     pause.disabled = false;
 });
 
 reset.addEventListener("click", () => {
+    if (timerInterval !== null) errorAudio.play();
     remaining = timerLength;
     clearInterval(timerInterval);
+    timerInterval = null;
     timer.classList = "";
     timer.classList.add("stop");
     start.disabled = false;
